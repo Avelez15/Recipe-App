c7393108e0924c0e7ef14725dea6fe5448b4e8de
Alejandro Velez <velez.alejandro4@gmail.com>
2023-07-27 14:30:06 -0400

    added styling,buttons, and image display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,10 +4,19 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-    recipes: Recipe[] = [
-      new Recipe('A Test Recipe', 'This is the description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
-    ];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is the description',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is the description',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+  ];
 }
