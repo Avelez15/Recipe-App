b013bda266bf27512c82faf7497360e6431b6baa
Alejandro Velez <velez.alejandro4@gmail.com>
2023-09-28 19:50:14 -0400

    added ability to fetch data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,54 +3,62 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { DataStorageService } from '../shared/data-storage.service';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Millionaire Spaghetti',
-      'This is the description',
-      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
-      [
-        new Ingredient('meat', 2),
-        new Ingredient('spaghetti', 1),
-        new Ingredient('onion', 1),
-        new Ingredient('garlic', 1),
-        new Ingredient('marinara', 1),
-        new Ingredient('butter', 1 / 4),
-        new Ingredient('cream cheese', 1),
-        new Ingredient('ricotta cheese', 1),
-        new Ingredient('parmesan cheese', 1),
-        new Ingredient('Mozzarella cheese', 1),
-        new Ingredient('parsley', 1),
-      ]
-    ),
-    new Recipe(
-      'Slammin Burger',
-      'A real tasty burger that will leave you wanting more!',
-      'https://i0.wp.com/www.aspicyperspective.com/wp-content/uploads/2020/05/Best-Hamburger-Patty-Recipe-14.jpg?resize=1132%2C1536&ssl=1',
-      [
-        new Ingredient('ground chuck', 1),
-        new Ingredient('crushed crackers', 1),
-        new Ingredient('Worcestershire sauce', 1),
-        new Ingredient('egg', 1),
-        new Ingredient('milk', 1),
-        new Ingredient('seasoning', 1),
-        new Ingredient('buns', 2),
-        new Ingredient('onions', 1),
-        new Ingredient('tomato', 2),
-        new Ingredient('lettuce', 1),
-        new Ingredient('mayo', 1),
-        new Ingredient('ketchup', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Millionaire Spaghetti',
+  //     'This is the description',
+  //     'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
+  //     [
+  //       new Ingredient('meat', 2),
+  //       new Ingredient('spaghetti', 1),
+  //       new Ingredient('onion', 1),
+  //       new Ingredient('garlic', 1),
+  //       new Ingredient('marinara', 1),
+  //       new Ingredient('butter', 1 / 4),
+  //       new Ingredient('cream cheese', 1),
+  //       new Ingredient('ricotta cheese', 1),
+  //       new Ingredient('parmesan cheese', 1),
+  //       new Ingredient('Mozzarella cheese', 1),
+  //       new Ingredient('parsley', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Slammin Burger',
+  //     'A real tasty burger that will leave you wanting more!',
+  //     'https://i0.wp.com/www.aspicyperspective.com/wp-content/uploads/2020/05/Best-Hamburger-Patty-Recipe-14.jpg?resize=1132%2C1536&ssl=1',
+  //     [
+  //       new Ingredient('ground chuck', 1),
+  //       new Ingredient('crushed crackers', 1),
+  //       new Ingredient('Worcestershire sauce', 1),
+  //       new Ingredient('egg', 1),
+  //       new Ingredient('milk', 1),
+  //       new Ingredient('seasoning', 1),
+  //       new Ingredient('buns', 2),
+  //       new Ingredient('onions', 1),
+  //       new Ingredient('tomato', 2),
+  //       new Ingredient('lettuce', 1),
+  //       new Ingredient('mayo', 1),
+  //       new Ingredient('ketchup', 1),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipe() {
     return this.recipes.slice(); //this will return a new array, which is an exact copy of this file. This prevents access to this array from outside, meaning we can only get this copy.
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipeById(id: number) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -16,5 +18,25 @@ export class DataStorageService {
       .subscribe((response) => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-recipebook-a5381-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
