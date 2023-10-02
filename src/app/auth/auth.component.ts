575a37d2faa198f41cab63b76678aa30eae4e7cd
Alejandro Velez <velez.alejandro4@gmail.com>
2023-10-02 01:30:31 -0400

    Started on authentication

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+    selector: 'app-auth',
+    templateUrl: './auth.component.html'
+})
+
+export class AuthComponent {
+     isLoginMode = true;
+
+     onSwithMode () {
+        this.isLoginMode = !this.isLoginMode;
+     }
+
+     onSubmit(form: NgForm) {
+        console.log(form.value);
+        form.reset();
+     }
+ 
+ 
+
+
+}
+
