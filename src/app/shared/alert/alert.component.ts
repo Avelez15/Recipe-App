6248d06f59f64c44979e65c651be522dc03b1297
Alejandro Velez <velez.alejandro4@gmail.com>
2023-10-12 03:16:59 -0400

    changed alert look

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,7 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-alert',
-    templateUrl: './alert.component.html'
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css'],
 })
-export class AlertComponent {}
+export class AlertComponent {
+  @Input() message: string;
+
+  closeAlert() {}
+}
