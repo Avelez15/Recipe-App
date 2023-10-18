b985894f951e8977c9c2f9e200ae666c7bdbd4bb
Alejandro Velez <velez.alejandro4@gmail.com>
2023-10-18 19:55:51 -0400

    added lazy loading configuration and mock logging service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
     path: 'shopping-list',
     loadChildren: () =>
       import('./shopping-list/shopping-list.module').then(
-        (m) => m.ShoppingListModule,
+        (m) => m.ShoppingListModule
       ),
   },
   {
@@ -22,7 +22,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
